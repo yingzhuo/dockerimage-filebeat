692cec052f18c990aa405ea6b55cb1ac13751bd6
yingzhuo <yingzhor@gmail.com>
2019-12-26 18:23:37 +0800

    2019-12-26 18:23:37

--- 5.6.16/docker-entrypoint.sh
+++ 5.6.16/docker-entrypoint.sh
@@ -2,8 +2,29 @@
 
 set -e
 
-# shellcheck disable=SC2039
-if [[ -f "/etc/filebeat/filebeat.yml" ]]; then
+if [ "${LOG_PATH}" = "" ]; then
+  echo "Env 'LOG_PATH' not set"
+  exit 1
+fi
+
+if [ "${ES_HOSTS}" = "" ]; then
+  echo "Env 'ES_HOSTS' not set"
+  exit 1
+fi
+
+if [ "${ES_PROTOCOL}" = "" ]; then
+  echo "Env 'ES_PROTOCOL' not set"
+  exit 1
+fi
+
+if [ "${ES_INDEX}" = "" ]; then
+  echo "Env 'ES_INDEX' not set"
+  exit 1
+fi
+
+docktool filegen -t="/etc/filebeat/template.yml" -o="/etc/filebeat/filebeat.yml"
+
+if [ -f "/etc/filebeat/filebeat.yml" ]; then
   exec filebeat -e -c /etc/filebeat/filebeat.yml
 else
   echo "[ERROR] no config file '/etc/filebeat/filebeat.yml'. "
